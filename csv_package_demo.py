a6def4109756917d73f46dd36c6efb86dbcb18c4
mprat <michele.pratusevich@gmail.com>
2013-11-03 16:04:58 +0200

    added main method

--- csv_package_demo.py
+++ csv_package_demo.py
@@ -1,14 +1,15 @@
 import csv
 
-f = open('30oysters.csv', 'rb')
-reader = csv.reader(f)
+if __name__ == "__main__":
+    f = open('30oysters.csv', 'rb')
+    reader = csv.reader(f)
 
-rownum = 0
-for row in reader:
-    if rownum == 0:
-        header = row
-    else:
-        print row
-    rownum += 1
+    rownum = 0
+    for row in reader:
+        if rownum == 0:
+            header = row
+        else:
+            print row
+        rownum += 1
 
-f.close()
+    f.close()
