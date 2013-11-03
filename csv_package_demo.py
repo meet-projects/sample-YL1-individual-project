415bc7b1799b702033adb5430469c19ec19d925a
mprat <michele.pratusevich@gmail.com>
2013-11-03 15:50:24 +0200

    csv package demo

---
+++ csv_package_demo.py
@@ -0,0 +1,14 @@
+import csv
+
+f = open('30oysters.csv', 'rb')
+reader = csv.reader(f)
+
+rownum = 0
+for row in reader:
+    if rownum == 0:
+        header = row
+    else:
+        print row
+    rownum += 1
+
+f.close()
