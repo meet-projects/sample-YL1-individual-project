b386b46f5757f37b41555046ba42697d4b6c2601
Michele Pratusevich <mprat@meet.mit.edu>
2013-11-04 18:48:37 +0200

    removing main method

--- csv_package_demo.py
+++ csv_package_demo.py
@@ -1,15 +1,14 @@
 import csv
 
-if __name__ == "__main__":
-    f = open('30oysters.csv', 'rb')
-    reader = csv.reader(f)
+f = open('30oysters.csv', 'rb')
+reader = csv.reader(f)
 
-    rownum = 0
-    for row in reader:
-        if rownum == 0:
-            header = row
-        else:
-            print row
-        rownum += 1
+rownum = 0
+for row in reader:
+if rownum == 0:
+    header = row
+else:
+    print row
+rownum += 1
 
-    f.close()
+f.close()
